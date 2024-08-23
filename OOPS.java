5799fef6f761e1865edbc2af3c51430b1fafb5ca
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2024-08-23 15:39:18 +0200

    Create OOPS.java

---
+++ OOPS.java
@@ -0,0 +1,29 @@
+public class OOPS {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+
+    }
+
+}
+
+class Pen {
+    // property and function
+    String color;
+    int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void setTip(int newTip) {
+        this.tip = newTip;
+    }
+}
