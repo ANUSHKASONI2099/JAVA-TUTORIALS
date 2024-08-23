5563f63f1bb773e2360a740c73ce9c346158d5ad
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2024-08-23 15:40:56 +0200

    Create recursion7.java

---
+++ recursion7.java
@@ -0,0 +1,20 @@
+public class recursion7 {
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        int fibnm1 = fib(n - 1);
+        int fibnm2 = fib(n - 2);
+        int fibn = fibnm1 + fibnm2;
+        return fibn;
+    }
+
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.println(fib(n));
+    }
+}
