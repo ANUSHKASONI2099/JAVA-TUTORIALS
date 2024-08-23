e2c78855b6a38ee23c60d0235f0aa645001c9cf5
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2024-08-23 15:36:10 +0200

    Create diagonalsum.java

---
+++ diagonalsum.java
@@ -0,0 +1,24 @@
+public class diagonalsum {
+    public static void diagonal_Sum(int matrix[][]) {
+        int sum = 0;
+        // for (int i = 0; i < matrix.length; i++) {
+        //     for (int j = 0; j < matrix[0].length; j++) {
+        //         if (i == j) {
+        //             sum += matrix[i][j];
+        //         } else if (i + j == matrix.length - 1) {
+        //             sum += matrix[i][j];
+        //         }
+
+        //     }
+        // }
+
+        for(int i=0; i<matrix.length; i++){
+            
+        }
+
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
